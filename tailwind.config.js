cfc59996fec9c77bcc5784573da1bc7eb643d27f
Nayanathara123 <nayanatharanadeeshani3@gmail.com>
2026-05-17 20:28:24 +0900

    All files commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}", // Add your React component paths here
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
